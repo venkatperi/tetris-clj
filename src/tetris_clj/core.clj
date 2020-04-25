6b343202049bb2d5808c03d21785f700761fdefa
Venkat Peri <venkatperi@gmail.com>
2020-04-25 13:18:43 -0700

    updates

--- src/tetris_clj/core.clj
+++ src/tetris_clj/core.clj
@@ -2,10 +2,47 @@
 (require '[tetris-clj.shapes :as s])
 (require '[tetris-clj.term :as t])
 
+(defn init-board
+  [width height val]
+  (vec (repeat height (vec (repeat width val)))))
+
 (def board-width 40)
 (def board-height 20)
+(def board (init-board board-width board-height 0))
 
-(defn start-game []
+(defn xyth
+  [matrix x y]
+  (nth (nth matrix y) x))
+
+(defn interferes
+  "returns true if the piece at (x,y) interferes with the board"
+  [board piece x y]
+  (let [bw (s/width board)
+        bh (s/height board)
+        w (s/width piece)
+        h (s/height piece)]
+    (cond
+      (< x 0) true
+      (< y 0) true
+      (>= (+ x w) bw) true
+      (>= (+ y h) bh) true
+      (every?
+        empty?
+        (keep-indexed
+          (fn
+            [dy row]
+            (keep-indexed
+              (fn
+                [dx pixel]
+                (if (> (+ pixel (xyth board (+ x dx) (+ y dy))) 1) pixel))
+              row))
+          piece)) false
+      :else true)))
+
+
+
+(defn start-game
+  []
   (t/start)
   (with-local-vars
     [getch nil                                              ; input from user
@@ -21,8 +58,8 @@
       (if (= 0 (mod @frame 10)) (var-set current-y (inc @current-y)))
       (var-set getch (t/get-key))
       (cond
-        (= @getch :right) (var-set current-x (inc @current-x))
-        (= @getch :left) (var-set current-x (dec @current-x))
+        (= @getch :right) (var-set current-x (if (interferes board @current-piece (inc @current-x) @current-y) @current-x (inc @current-x)))
+        (= @getch :left) (var-set current-x (if (interferes board @current-piece (dec @current-x) @current-y) @current-x (dec @current-x)))
         (= @getch :up) (var-set current-piece (s/rotate-90-cc @current-piece))
         ))
     (t/stop)))
