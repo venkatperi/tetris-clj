315da9283ab0fd2acb6152df02c9e603a64b71ce
Venkat Peri <venkatperi@gmail.com>
2020-04-26 11:37:11 -0700

    drop piece

--- src/tetris_clj/core.clj
+++ src/tetris_clj/core.clj
@@ -33,6 +33,15 @@
         grown-piece (s/grow-mat piece x y bw bh)]
     (s/add-mat board grown-piece)))
 
+(defn drop-piece
+  [board piece x y]
+  (let [bh (s/height board)
+        h (s/height piece)]
+    (first
+      (drop-while
+        #(interferes board piece x %)
+        (map (partial - bh) (range h bh))))))
+
 (def board-width 40)
 (def board-height 20)
 
@@ -44,7 +53,7 @@
      current-piece (s/get-random-piece)                     ; the current piece
      current-x (/ (- board-width 2) 2)                      ; piece x position
      current-y 0]                                           ; piece y position
-    (loop [frame 0]
+    (loop [frame 1]
       (t/cls)
       (t/render-piece @board 0 0)
       (t/render-piece @current-piece @current-x @current-y)
@@ -55,6 +64,7 @@
           (do
             (var-set board (write-piece-to-board @board @current-piece @current-x @current-y))
             (var-set current-y 0)
+            (var-set current-x (/ (- board-width 2) 2))
             (var-set current-piece (s/get-random-piece)))
           (var-set current-y (inc @current-y))))
 
@@ -62,6 +72,7 @@
         (cond
           (= :right getch) (var-set current-x (if (interferes @board @current-piece (inc @current-x) @current-y) @current-x (inc @current-x)))
           (= :left getch) (var-set current-x (if (interferes @board @current-piece (dec @current-x) @current-y) @current-x (dec @current-x)))
+          (= :down getch) (var-set current-y (drop-piece @board @current-piece @current-x @current-y))
           (= :up getch) (var-set current-piece (s/rotate-90-cc @current-piece)))
         (if (not (= \q getch))
           (recur (inc frame))))))
