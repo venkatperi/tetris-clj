b668e08b67a0d8252485715d9e94b3502c8c6b39
Venkat Peri <venkatperi@gmail.com>
2020-04-26 08:19:25 -0700

    updates

--- src/tetris_clj/core.clj
+++ src/tetris_clj/core.clj
@@ -2,17 +2,6 @@
 (require '[tetris-clj.shapes :as s])
 (require '[tetris-clj.term :as t])
 
-(defn init-board
-  [width height val]
-  (vec (repeat height (vec (repeat width val)))))
-
-(def board-width 40)
-(def board-height 20)
-
-(defn xyth
-  [matrix x y]
-  (nth (nth matrix y) x))
-
 (defn interferes
   "returns true if the piece at (x,y) interferes with the board"
   [board piece x y]
@@ -33,69 +22,49 @@
             (keep-indexed
               (fn
                 [dx pixel]
-                (if (> (+ pixel (xyth board (+ x dx) (+ y dy))) 1) pixel))
+                (if (> (+ pixel (s/xyth board (+ x dx) (+ y dy))) 1) pixel))
               row))
           piece)) false
       :else true)))
 
-(defn take-between [n m coll]
-  (cond
-    (< m n) []
-    :else (->> coll (take n) (take (- m n)))))
-
-(defn zero-mat [w h]
-  (->> (repeat w 0) vec (repeat h) vec))
-
-(defn grow-mat
-  [piece x y bw bh]
-  (let [w (s/width piece)
-        h (s/height piece)]
-    (concat
-      (zero-mat bw y)
-      (map
-        (fn [row]
-          (concat
-            (vec (repeat x 0))
-            row
-            (vec (repeat (- bw (+ x w)) 0)))) piece)
-      (zero-mat bw (- bh (+ y h))))))
-
-(defn add-mat [m1 m2]
-  (mapv #(mapv + %1 %2) m1 m2))
-
 (defn write-piece-to-board [board piece x y]
   (let [bw (s/width board)
         bh (s/height board)
-        grown-piece (grow-mat piece x y bw bh)]
-    (add-mat board grown-piece)))
+        grown-piece (s/grow-mat piece x y bw bh)]
+    (s/add-mat board grown-piece)))
+
+(def board-width 40)
+(def board-height 20)
 
 (defn start-game
   []
   (t/start)
   (with-local-vars
-    [getch nil                                              ; input from user
-     frame 0                                                ; frame counter
-     board (init-board board-width board-height 0)          ; the board
-     current-piece s/j                                      ; the current piece
-     current-x 10                                           ; piece x position
-     current-y 1]                                           ; piece y position
-    (while (not (= @getch \q))
+    [board (s/zero-mat board-width board-height)            ; the board
+     current-piece (s/get-random-piece)                     ; the current piece
+     current-x (/ (- board-width 2) 2)                      ; piece x position
+     current-y 0]                                           ; piece y position
+    (loop [frame 0]
       (t/cls)
       (t/render-piece @board 0 0)
       (t/render-piece @current-piece @current-x @current-y)
       (Thread/sleep 100)
-      (var-set frame (inc @frame))
-      (if (= 0 (mod @frame 10))
+
+      (if (= 0 (mod frame 10))
         (if (interferes @board @current-piece @current-x (inc @current-y))
           (do
             (var-set board (write-piece-to-board @board @current-piece @current-x @current-y))
-            (var-set current-y 1))
+            (var-set current-y 1)
+            (var-set current-piece (s/get-random-piece)))
           (var-set current-y (inc @current-y))))
-      (var-set getch (t/get-key))
-      (cond
-        (= @getch :right) (var-set current-x (if (interferes @board @current-piece (inc @current-x) @current-y) @current-x (inc @current-x)))
-        (= @getch :left) (var-set current-x (if (interferes @board @current-piece (dec @current-x) @current-y) @current-x (dec @current-x)))
-        (= @getch :up) (var-set current-piece (s/rotate-90-cc @current-piece))
-        ))
-    (t/stop)))
+
+      (let [getch (t/get-key)]
+        (cond
+          (= :right getch) (var-set current-x (if (interferes @board @current-piece (inc @current-x) @current-y) @current-x (inc @current-x)))
+          (= :left getch) (var-set current-x (if (interferes @board @current-piece (dec @current-x) @current-y) @current-x (dec @current-x)))
+          (= :up getch) (var-set current-piece (s/rotate-90-cc @current-piece)))
+        (if (not (= \q getch))
+          (recur (inc frame))))))
+
+  (t/stop))
 
--- src/tetris_clj/shapes.clj
+++ src/tetris_clj/shapes.clj
@@ -8,6 +8,11 @@
 (def s [[0 1 1] [1 1 0]])
 (def t [[0 1 0] [1 1 1]])
 
+(def pieces [i o l j z s t])
+
+(defn get-random-piece []
+  (let [total (count pieces)]
+    (nth pieces (rand-int total) )))
 
 (defn height [rows]
   (count rows))
@@ -20,3 +25,30 @@
 
 (defn rotate-90-cc [piece]
   (reverse (transpose piece)))
+
+(defn xyth
+  [matrix x y]
+  (nth (nth matrix y) x))
+
+(defn zero-mat [w h]
+  (->> (repeat w 0) vec (repeat h) vec))
+
+(defn grow-mat
+  [piece x y bw bh]
+  (let [w (width piece)
+        h (height piece)]
+    (concat
+      (zero-mat bw y)
+      (map
+        (fn [row]
+          (concat
+            (vec (repeat x 0))
+            row
+            (vec (repeat (- bw (+ x w)) 0)))) piece)
+      (zero-mat bw (- bh (+ y h))))))
+
+(defn add-mat [m1 m2]
+  (mapv #(mapv + %1 %2) m1 m2))
+
+
+
